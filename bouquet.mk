b116285d757c3350429d53142aab0927d1993f81
Nope Nope <kingdutchisbad+12@proton.me>
2024-05-01 16:55:27 +0530

    bouquet: configs: Trick google into thinking we can have unlimited photos

--- bouquet.mk
+++ bouquet.mk
@@ -61,6 +61,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+# Unlimited GPhotos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
