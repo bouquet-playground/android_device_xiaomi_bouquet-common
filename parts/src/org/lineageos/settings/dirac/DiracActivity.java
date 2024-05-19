a17af251643d200508e153ad3b0e0c1237b676fc
Nope Nope <kingdutchisbad+12@proton.me>
2024-05-19 23:04:05 +0530

    Doze --> XiaomiParts
    Imported from https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/1df69badc73fafdcaf3554124b628d779c37d9ad/parts

--- parts/src/org/lineageos/settings/dirac/DiracActivity.java
+++ parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015-2016 The CyanogenMod Project
- *               2017,2022 The LineageOS Project
+ * Copyright (C) 2018,2021-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,21 +14,22 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package org.lineageos.settings.dirac;
 
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
-public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
+public class DiracActivity extends CollapsingToolbarBaseActivity {
 
-    private static final String TAG_DOZE = "doze";
+    private static final String TAG_DIRAC = "dirac";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
-                new DozeSettingsFragment(), TAG_DOZE).commit();
+        getFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new DiracSettingsFragment(), TAG_DIRAC).commit();
     }
 }
